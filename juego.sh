c92336b85ddfab3a8fd716677701e7c31315a43c
Oslolu <oslolu01@floridauniversitaria.es>
2023-01-31 16:30:20 +0100

    Instrucciones

---
+++ juego.sh
@@ -0,0 +1,7 @@
+function instrucciones {
+  echo "Instrucciones del juego del 7 y 1/2:"
+  echo "==================================="
+  echo "El objetivo del juego es conseguir una puntuación lo más cercana posible a 7.5 sin pasarse."
+  echo "Cada carta tiene un valor: las cartas numeradas valen su número, las figuras valen medio punto y el as vale 1 punto."
+  echo "El jugador que se acerque más a 7.5 gana la partida."
+}
