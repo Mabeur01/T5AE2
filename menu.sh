9f9605b9e87c21e69a2d4bf25a20378e9f1719ac
alarmi04 <114494727+alarmi04@users.noreply.github.com>
2023-01-31 16:34:03 +0100

    menu actualizado 1

--- menu.sh
+++ menu.sh
@@ -1,2 +1,36 @@
 #!/bin/bash
 
+opcion=0
+
+while [ $opcion -ne "4" ]
+do
+    echo ""
+    echo "===============JUEGO 7 Y MEDIO.================"
+    echo ""
+    echo "-------------MENU DE OPCIONES------------------"
+    echo "1: INICIAR JUEGO."
+    echo "2: INSTRUCCIONES."
+    echo "3: NÚMERO DE JUGADORES."
+    echo "4: SALIR DEL JUEGO."
+    echo "----------------------------------------------"
+    echo " "
+    read -p "Selecciona una opción (1..4): " opcion
+    echo " "
+
+    case $opcion in 
+    "1")
+    ##AÑADIR LLAMADA A SCRIPT
+    ;;
+    "2")
+    ##AÑADIR LLAMADA A SCRIPT
+    ;;
+    "3")
+    ##AÑADIR LLAMADA A SCRIPT
+    ;;
+    "4")
+    echo "FIN DEL JUEGO."
+    break
+    ;;
+    esac
+
+done
