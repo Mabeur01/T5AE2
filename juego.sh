784b86009d1836e36929795b2620e7655dba6251
Oslolu <oslolu01@floridauniversitaria.es>
2023-02-03 15:17:46 +0100

    Actualizacion de las instrucciones

--- juego.sh
+++ juego.sh
@@ -1,7 +1,8 @@
 function instrucciones {
   echo "Instrucciones del juego del 7 y 1/2:"
   echo "==================================="
-  echo "El objetivo del juego es conseguir una puntuación lo más cercana posible a 7.5 sin pasarse."
-  echo "Cada carta tiene un valor: las cartas numeradas valen su número, las figuras valen medio punto y el as vale 1 punto."
-  echo "El jugador que se acerque más a 7.5 gana la partida."
+  echo "El objetivo del juego es conseguir una puntuación lo más cercana posible a 7 y 1/2 sin pasarse."
+  echo "Cada carta de la baraja tiene un valor: las cartas numeradas valen su número(numero indicado en la carta), 
+  las figuras valen medio punto y el as vale 1 punto."
+  echo "El jugador que saque la puntuación más cercana a 7 y 1/2 sin pasarse gana la partida."
 }
