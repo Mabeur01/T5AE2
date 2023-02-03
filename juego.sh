11bd016e862bf8565697d289b7e5e187430ab6a9
alarmi04 <114494727+alarmi04@users.noreply.github.com>
2023-02-03 17:24:14 +0100

    actualizando las instrucciones

--- juego.sh
+++ juego.sh
@@ -1,8 +1,7 @@
-function instrucciones {
+
   echo "Instrucciones del juego del 8:"
   echo "==================================="
-  echo "El objetivo del juego es conseguir una puntuación lo más cercana posible a 8 sin pasarse."
-  echo "Cada carta de la baraja tiene un valor: las cartas numeradas valen su número(numero indicado en la carta), 
-  las figuras valen 1 punto y el as también vale 1 punto."
-  echo "El jugador que saque la puntuación más cercana a 8 sin pasarse gana la partida."
-}
+  echo "1) El objetivo del juego es conseguir una puntuación lo más cercana posible a 8 sin pasarse."
+  echo "2) Cada carta de la baraja tiene un valor: las cartas numeradas valen su número(numero indicado en la carta), 
+   las figuras valen 1 punto y el as también vale 1 punto."
+  echo "3) El jugador que saque la puntuación más cercana a 8 sin pasarse gana la partida."
